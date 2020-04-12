ea1cc0e57c3009473fb47a6cd2d3fb9250399d33
Andrew Kim <andrewswkim47@gmail.com>
2020-04-12 14:44:45 -0700

    add app, ticket, and ticket list components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <ReactFragment>
       <Header />
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+     <TicketList />
     </ReactFragment>
   );
 }
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,7 +3,18 @@ import Ticket from "./Ticket.jsx";
 
 function TicketList() {
     return (
-        <Ticket />
+        <React.Fragment>
+        <Ticket 
+            location="3A"
+            names="Thanto and Haley"
+            issue="Firebase will not save record!"
+        />
+        <Ticket
+            location ="4B"
+            names="Sleater and Kinney"
+            issue="Prop types are throwing an error."
+        />
+        </React.Fragment>
     );
 }
 
