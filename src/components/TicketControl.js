8c9116a5f2a70f11b0966b6911861b4a47efe18c
Andrew Kim <andrewswkim47@gmail.com>
2020-04-19 13:38:45 -0700

    add working button to go toggle back and forth.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,13 +6,32 @@ class TicketControl extends React.Component {
   
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
+    let currentVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
+    } else {
+      currentVisibleState = <TicketList />
+      buttonText  = "Add Ticket";
+    }
     return (
       <React.Fragment>
-
+        {currentVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
